bf0595007fa34e4a6d5614b8091e47a457f13b8c
hulin <hulin2021@gmail.com>
2022-12-01 20:33:36 +0100

    init repo

---
+++ src/main/kotlin/Day1.1.kt
@@ -0,0 +1,16 @@
+import java.io.File
+import kotlin.math.max
+
+fun main() {
+    var result = 0
+    var current = 0
+    File("day1.txt").forEachLine {
+        if (it.isEmpty()) {
+            result = max(result, current)
+            current = 0
+        } else {
+            current += it.toInt()
+        }
+    }
+    println("result: $result")
+}
---
+++ src/main/kotlin/Day1.2.kt
@@ -0,0 +1,17 @@
+import java.io.File
+import java.nio.file.Paths
+import kotlin.math.max
+
+fun main() {
+    var result = mutableSetOf<Int>()
+    var current = 0
+    File("day1.txt").forEachLine {
+        if (it.isEmpty()) {
+            result.add(current)
+            current = 0
+        } else {
+            current += it.toInt()
+        }
+    }
+    println("result: " + result.sortedDescending().take(3).sum())
+}
