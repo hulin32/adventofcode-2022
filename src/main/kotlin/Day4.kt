f815baddb8709cacaec932a552582aab04a72726
hulin <hulin2021@gmail.com>
2022-12-04 17:37:14 +0100

    add day4

---
+++ src/main/kotlin/Day4.kt
@@ -0,0 +1,30 @@
+import java.util.*
+
+fun main() {
+    val input = Utils.getResourceFile("day4.txt")!!
+    fun part1(): Int {
+        return input
+            .readLines()
+            .filter {
+                val(l, r) = it.split(",")
+                val(lLeft, lRight) = l.split("-").map { itInner -> itInner.toInt() }
+                val(rLeft, rRight) = r.split("-").map { itInner -> itInner.toInt() }
+                (lLeft <= rLeft && rRight <= lRight) || (rLeft <= lLeft && lRight <= rRight)
+            }
+            .size
+    }
+    fun part2(): Int {
+        return input
+            .readLines()
+            .filter {
+                val(l, r) = it.split(",")
+                val(lLeft, lRight) = l.split("-").map { itInner -> itInner.toInt() }
+                val(rLeft, rRight) = r.split("-").map { itInner -> itInner.toInt() }
+                !((lRight < rLeft) || (rRight < lLeft))
+            }
+            .size
+    }
+    println(part1())
+    println(part2())
+}
+
