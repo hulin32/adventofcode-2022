4e4e7e348555f9b3490fb75be06d54f4f51ed58e
hulin <hulin2021@gmail.com>
2022-12-08 07:45:35 +0100

    add day6

---
+++ src/main/kotlin/Day6.kt
@@ -0,0 +1,37 @@
+fun main() {
+    val input = Utils.getResourceFile("day6.txt")!!
+
+    fun findDistinctCharacters(num: Int): Int {
+        val text = input.readText()
+        var first = 1
+        var tail = 0
+        while (first != tail) {
+            if ((first - tail) == num) {
+                return first
+            }
+            val substr = text.substring(tail, first)
+            val newChar = text[first]
+            val foundIdx = substr.indexOf(newChar)
+            if (foundIdx >= 0) {
+                tail += foundIdx + 1
+            } else {
+                first += 1
+            }
+            if (tail == first && tail.toLong() != (input.length() - 1)) {
+                first += 1
+            }
+        }
+        return first
+    }
+
+    fun part1(): Int {
+        return findDistinctCharacters(4)
+    }
+
+    fun part2(): Int {
+        return findDistinctCharacters(14)
+    }
+    println(part1())
+    println(part2())
+}
+
