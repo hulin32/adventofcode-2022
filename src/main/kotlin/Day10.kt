70d7ebe62a766bd92f233ed6b347ead7b9c43d12
hulin <hulin2021@gmail.com>
2022-12-15 22:19:52 +0100

    day11 copyed

--- src/main/kotlin/Day10.kt
+++ src/main/kotlin/Day10.kt
@@ -23,6 +23,7 @@ fun main() {
                     val v = line.split(' ').last().toInt()
                     result += v
                 }
+
                 else -> {
                     checkSignalStrength()
                 }
@@ -31,10 +32,52 @@ fun main() {
         return signalResult
     }
 
-    fun part2(): Int {
-        return 1
+    // ref https://github.com/pavlo-dh/aoc-2022-in-kotlin/blob/main/src/Day10.kt
+    fun part2(): String {
+        var cycle = 0
+        var x = 1
+        var spritePosition = 0..2
+        val screen = Array(6) { CharArray(40) { '.' } }
+
+        fun draw() {
+            val row = cycle / 40
+            val column = cycle - row * 40
+            if (column in spritePosition) {
+                screen[row][column] = '#'
+            }
+        }
+
+        fun noop() {
+            draw()
+            cycle++
+        }
+
+        fun add(line: String) {
+            repeat(2) {
+                noop()
+            }
+            val v = line.split(' ').last().toInt()
+            x += v
+            spritePosition = x - 1..x + 1
+        }
+
+        fun printScreen() = buildString {
+            for (i in screen.indices) {
+                for (j in screen.first().indices) {
+                    append(screen[i][j])
+                }
+                if (i != screen.lastIndex) appendLine()
+            }
+        }
+
+        input.forEachLine { line ->
+            if (line.startsWith('n')) noop()
+            else add(line)
+        }
+
+        return printScreen()
     }
     println(part1())
-//    println(part2())
+    println(part2())
 }
 
